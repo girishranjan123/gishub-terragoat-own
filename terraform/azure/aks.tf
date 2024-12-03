4fe6f14a14c7e1c0a991854ea1b98b99b6c082ba
GirishRanjan <134987016+GirishRanjan@users.noreply.github.com>
2024-12-03 10:26:02 +0800

    Update aks.tf

--- terraform/azure/aks.tf
+++ terraform/azure/aks.tf
@@ -1,4 +1,9 @@
 resource azurerm_kubernetes_cluster "k8s_cluster" {
+# Test case for check skip via comment
+# checkov:skip=CKV_AZURE_172: not required
+# checkov:skip=CKV_AZURE_4: not needed
+# checkov:skip=CKV_AZURE_227: Dont want
+# checkov:skep=CKV_AZURE_115: not necessary
   dns_prefix          = "terragoat-${var.environment}"
   location            = var.location
   name                = "terragoat-aks-${var.environment}"
